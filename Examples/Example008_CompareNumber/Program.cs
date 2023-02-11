caf5e8f67746c4d7f3103ac7a3d87b6553582934
Denis.Biryuchevskiy <DenisBiryuchevskiy@gmail.com>
2023-02-11 10:11:33 +0300

    Когда же это все ляжет в голове

---
+++ Examples/Example008_CompareNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ведите два числа для сравнения");
+Console.Write("Ведите число А - ");
+int numberA = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Ведите число В - ");
+int numberB = int.Parse(Console.ReadLine() ?? "0");
+if (numberA > numberB)
+{
+        Console.WriteLine("Max:"+numberA);
+        Console.WriteLine("Min:"+numberB);
+}
+else if (numberA < numberB)
+{
+        Console.WriteLine("Max:"+numberB);
+        Console.WriteLine("Min:"+numberA);
+}
+else
+{
+    Console.WriteLine("число" +numberA+" и " +numberB+ "равны");
+}
